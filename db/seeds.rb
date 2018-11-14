4c6b57bea7ac6d903cdb271ccbfcbacc3a885363
github username <github email address>
2018-11-14 08:59:44 -0500

    added links to the user show page.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,22 +9,23 @@
 #rails db:reset to reseed re migrate and everything !!!
 
 
-User.create(name: "Sydney", username: "SydDawgg")
-User.create(name: "Yoan", username: "YoanDawgg")
-
 Location.create(city: "NYC", country: "USA")
 Location.create(city: "Los Angeles", country: "USA")
 
 Landmark.create(name: "Empire State Building", description: "Lights up at night", img_url: "empire.jpg", location_id: 1)
+Landmark.create(name: "Statue of Liberty", description: "Big ass bronze statue, giving to us by the French", img_url: "statue.jpg", location_id: 1)
+Landmark.create(name: "Charging Bull", description: "Bull that is about to run over a child in the financial district of New York", img_url: "bull.jpg", location_id: 1)
+
 Landmark.create(name: "Hollywood Sign", description: "Don't Climb", img_url: "hollywood.jpg", location_id: 2)
+Landmark.create(name: "Hollywood Walk of Fame", description: "a bunch of stars on the sidewalk with some famouse person's name", img_url: "hollywood_walk.jpg", location_id: 2)
 
 Restaurant.create(name: "Blue Smoke", cuisine: "BBQ", location_id: 1)
 Restaurant.create(name: "Urth Cafe", cuisine: "Earthy", location_id: 2)
 
-Trip.create(location_id: 2, user_id: 1)
-Trip.create(location_id: 1, user_id: 2)
+# Trip.create(user_id: 1, location_id: 2)
+# Trip.create(user_id: 2, location_id: 1)
 
-TripLandmark.create(trip_id: 1, landmark_id: 1)
+# TripLandmark.create(trip_id: 1, landmark_id: 1)
 # User.first.trips.first.landmarks
 
-TripRestaurant.create(trip_id: 2, restaurant_id: 2)
+# TripRestaurant.create(trip_id: 2, restaurant_id: 2)
