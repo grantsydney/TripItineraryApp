9629e418e5df306dbe60fece456d8ebe22692bd8
Yoan Ante <yoanante@gmail.com>
2018-11-15 16:13:57 -0500

    db styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,21 +11,23 @@
 
 Location.create(city: "NYC", country: "USA")
 Location.create(city: "Los Angeles", country: "USA")
-
-Landmark.create(name: "Empire State Building", description: "Lights up at night", img_url: "empire.jpg", location_id: 1)
-Landmark.create(name: "Statue of Liberty", description: "Big ass bronze statue, giving to us by the French", img_url: "statue.jpg", location_id: 1)
-Landmark.create(name: "Charging Bull", description: "Bull that is about to run over a child in the financial district of New York", img_url: "bull.jpg", location_id: 1)
-
-Landmark.create(name: "Hollywood Sign", description: "Don't Climb", img_url: "hollywood.jpg", location_id: 2)
-Landmark.create(name: "Hollywood Walk of Fame", description: "a bunch of stars on the sidewalk with some famouse person's name", img_url: "hollywood_walk.jpg", location_id: 2)
-
-Restaurant.create(name: "Blue Smoke", cuisine: "BBQ", location_id: 1)
-Restaurant.create(name: "Urth Cafe", cuisine: "Earthy", location_id: 2)
-
-# Trip.create(user_id: 1, location_id: 2)
-# Trip.create(user_id: 2, location_id: 1)
-
-# TripLandmark.create(trip_id: 1, landmark_id: 1)
-# User.first.trips.first.landmarks
-
-# TripRestaurant.create(trip_id: 2, restaurant_id: 2)
+Location.create(city: "Minnesota", country: "USA")
+Location.create(city: "Austin", country: "USA")
+Location.create(city: "Dallas", country: "USA")
+Location.create(city: "Nashville", country: "USA")
+Location.create(city: "Miami", country: "USA")
+Location.create(city: "Charleston", country: "USA")
+Location.create(city: "Savannah", country: "USA")
+Location.create(city: "San Francisco", country: "USA")
+Location.create(city: "Seatle", country: "USA")
+Location.create(city: "Houston", country: "USA")
+Location.create(city: "Chicago", country: "USA")
+Location.create(city: "Pittsburgh", country: "USA")
+Location.create(city: "Boston", country: "USA")
+Location.create(city: "Rome", country: "Italy")
+Location.create(city: "London", country: "UK")
+Location.create(city: "Paris", country: "Frace")
+Location.create(city: "Beijing", country: "China")
+Location.create(city: "Tokyo", country: "Japan")
+Location.create(city: "Delhi", country: "India")
+Location.create(city: "Moscow", country: "Russia")
