816690d6bd8665a042cc41316e4d3790f4c6636a
Yoan Ante <yoanante@gmail.com>
2019-02-03 11:46:39 -0500

    damn asset pipeline

--- config/routes.rb
+++ config/routes.rb
@@ -2,37 +2,38 @@ Rails.application.routes.draw do
 
 
 scope path: 'travelpal' do 
-get '/', to: "welcome#index"
+  get '/', to: "welcome#index"
 
-resources :users, only: [:new, :create, :show, :index]
-resources :restaurants, only: [:index, :show]
-resources :landmarks, only: [:index, :show]
-resources :trips, only: [:new, :create, :show, :destroy]
-resources :trip_landmarks, only: [:new, :create, :destroy]
-resources :trip_restaurants, only: [:new, :create, :destroy]
-resources :places, only: [:show, :destroy]
-
-
-get '/places', to: "places#index"
-post '/places/:id', to: "places#custom", as: :custom
+  resources :users, only: [:new, :create, :show, :index]
+  resources :restaurants, only: [:index, :show]
+  resources :landmarks, only: [:index, :show]
+  resources :trips, only: [:new, :create, :show, :destroy]
+  resources :trip_landmarks, only: [:new, :create, :destroy]
+  resources :trip_restaurants, only: [:new, :create, :destroy]
+  resources :places, only: [:show, :destroy]
 
 
-post '/users/:id', to: "users#show"
-
-get '/locations/:location_id/restaurants', to: "locations#location_restaurants"
-get '/locations/:location_id/landmarks', to: "locations#location_landmarks"
-
-post 'landmarks/:id', to: "trip_landmarks#create"
-post 'restaurants/:id', to: "trip_restaurants#create"
-post '/places', to: "places#create"
-
-#custom session route for login
-get '/login', to: 'sessions#new', as: :login
-post '/login', to: 'sessions#create'
-delete '/logout', to: 'sessions#destroy', as: :logout
+  get '/places', to: "places#index"
+  post '/places/:id', to: "places#custom", as: :custom
 
 
-get '*path' => redirect('/')
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  post '/users/:id', to: "users#show"
+
+  get '/locations/:location_id/restaurants', to: "locations#location_restaurants"
+  get '/locations/:location_id/landmarks', to: "locations#location_landmarks"
+
+  post 'landmarks/:id', to: "trip_landmarks#create"
+  post 'restaurants/:id', to: "trip_restaurants#create"
+  post '/places', to: "places#create"
+
+  #custom session route for login
+  get '/login', to: 'sessions#new', as: :login
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy', as: :logout
+
+
+  get '*path' => redirect('/')
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
+  config.assets.prefix="/travelpal/assets"
 end
